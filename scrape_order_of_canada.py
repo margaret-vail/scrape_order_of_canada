1144b902b8995232b8ca3170d998b69aa6e4ed50
Margaret Vail <47865972+margaret-vail@users.noreply.github.com>
2021-12-08 20:09:52 -0400

    about to make big changes

--- scrape_order_of_canada.py
+++ scrape_order_of_canada.py
@@ -11,11 +11,33 @@ log_file = os.path.join(dirname, 'logfile_' + datetime.now().strftime("%Y%m%dt%H
 url = 'https://www.gg.ca/en/honours/recipients?f%5B0%5D=honour_type_id%3A%22146%22'# Create object page
 base_url = 'https://www.gg.ca'
 
+member_url = "https://www.gg.ca/en/honours/recipients?f%5B0%5D=honour_type_id%3A%22146%22&f%5B1%5D=honour_level_id%3A%22149%22"
+officer_url = "https://www.gg.ca/en/honours/recipients?f%5B0%5D=honour_type_id%3A%22146%22&f%5B1%5D=honour_level_id%3A%22148%22"
+companion_url = ""
+
+# Test number of pages
+total_num_pages_member = 2
+total_num_pages_officer = 2
+total_num_pages_companion = 2
+
+# Real number of pages
+#total_num_pages_member = 195
+#total_num_pages_officer = 99
+#total_num_pages_companion = 20
+
+# CSV headers
+headers = ["id", "name", "location", "honour", "level", "awarded_date", "invested_date", "full_name", "bio", "status", "detail_url"]
+
+# Create a dataframe
+mydata = pd.DataFrame(columns = headers)
+
+def sameForEveryone(i):
+
+
 i = 1
 
-while(i <= 2):
-
-    #f = open(log_file, "a")
+# Get Members
+while(i <= total_num_pages_member):
 
     if i == 0:
         page = requests.get(url)
@@ -28,12 +50,6 @@ while(i <= 2):
 
     # Obtain information from tag <table>
     table1 = soup.find('table')
-
-    # CSV headers
-    headers = ["name", "location", "honour", "membership", "awarded_date", "invested_date", "full_name", "bio", "detail_url"]
-
-    # Create a dataframe
-    mydata = pd.DataFrame(columns = headers)
 
     # Create a for loop to fill mydata
     for j in table1.find_all('tr')[1:]:
@@ -56,14 +72,18 @@ while(i <= 2):
 
         # Get information from Detail html page
         more_info_url = row_data[0].contents[1].attrs['href']
-        url2 = base_url+more_info_url
+        # Real URL
+        #url2 = base_url+more_info_url
+        # Test a particular case URL
+        url2 = "https://www.gg.ca/en/honours/recipients/146-60"
+        
         page2 = requests.get(url2)
         soup2 = BeautifulSoup(page2.text, 'lxml')
 
         try:
-            membership = soup2.h3.text.strip()
+            level = soup2.h3.text.strip()
         except:
-            membership = 'Possible Error'
+            level = 'Possible Error'
         try:
             awarded_date = " ".join(soup2.find(text=re.compile('Awarded on')).strip().split()).replace("Awarded on: ","")
         except:
@@ -80,16 +100,24 @@ while(i <= 2):
             bio = soup2.p.text.strip()
         except:
             bio = 'Possible Error'
-
+        try:
+            status = soup2.find(text=re.compile('Deceased'))
+            if status is None:
+                status = 'Alive'
+            else:
+                status = soup2.find(text=re.compile('Deceased')).strip()
+        except:
+            status = 'Possible Error'
+        
+        uid = more_info_url.replace("/en/honours/recipients/","")
+        
         detail_url = url2
 
-        row = [name, location, honour, membership, awarded_date, invested_date, full_name, bio, detail_url]
-
-        #row = [i.text.strip() for i in row_data]
-        length = len(mydata)
-        mydata.loc[length] = row
+        row = [uid, name, location, honour, level, awarded_date, invested_date, full_name, bio, status, detail_url]
 
         i = i+1
-    #f.close()
+
+    length = len(mydata)
+    mydata.loc[length] = row
     
 mydata.to_csv(os.path.join(dirname, 'test.csv'), index=False)
